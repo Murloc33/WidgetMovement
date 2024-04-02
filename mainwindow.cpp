221ccdb824403c1f4e36d97134f746ca71112c59
nikit <a.myakonkikh@yandex.ru>
2024-04-02 15:28:20 +0500

    WidgetMovement V2.0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,27 +10,25 @@ MainWindow::MainWindow(QWidget *parent)
 	resize(300, 600);
 
 	m_timer = new QTimer(this);
-	int lowerBound = 100;
-	int upperBound = 1000;
 
-	m_timer->setInterval(m_randomizer.bounded(lowerBound,upperBound));
+	m_timer->setInterval(m_randomizer.bounded(100,1000));
 	m_timer->start();
 
-	connect(m_timer, &QTimer::timeout, [lowerBound, upperBound, this]
+	connect(m_timer, &QTimer::timeout, [this]
 	{
-		m_timer->setInterval(m_randomizer.bounded(lowerBound,upperBound));
+		m_timer->setInterval(m_randomizer.bounded(100,1000));
 		m_timer->start();
 
 		auto pb = new QPushButton("*", this);
 		pb->setMouseTracking(true);
 
-		qint32 x = m_randomizer.bounded(30, this->width() - 30);
-		qint32 y = 10;
+		const qint32 startPositionX = m_randomizer.bounded(30, this->width() - 30);
+		const qint32 startPositionY = 10;
 
-		pb->setGeometry(x, y, 25, 25);
+		pb->setGeometry(startPositionX, startPositionY, 25, 25);
 		pb->show();
 
-		auto speed = m_randomizer.bounded(1,10);
+		const auto speed = m_randomizer.bounded(1,10);
 
 		auto moveTimer = new QTimer(pb);
 		moveTimer->setInterval(100);
